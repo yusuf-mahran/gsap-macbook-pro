40349e5e4eb8eded53e61d307024452c92e53d1a
Yusuf Mahran <yusuf.mahran.100@gmail.com>
2026-03-10 11:46:19 +0200

    feat: 🦶 implementing footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,30 @@
+import { footerLinks } from '@/constants';
+import Image from 'next/image';
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className="info">
+        <p>
+          More ways to shop: Find an Apple Store or other retailer near you. Or
+          call 000800 040 1966.
+        </p>
+        <Image src="/logo.svg" alt="Apple Logo" width={50} height={50} />
+      </div>
+
+      <hr />
+
+      <div className="links">
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+
+        <ul>
+          {footerLinks.map(({ label, link }) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 }
